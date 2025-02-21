73fdd6961fc38f4274600237fb85545ebab81392
09ahmad <shkhahmad64@gmail.com>
2025-02-21 11:10:13 +0530

    My Neovim config for development (first Commit )

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,33 @@
+local conform = require("conform")
+
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    css = { "prettier" },
+    html = { "prettier" },
+    javascript = { "prettier" },
+    typescript = { "prettier" },
+    json = { "prettier" },
+    tsx = { "prettier" },
+    jsx = { "prettier" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  formatters = {
+    prettier = {
+      command = "prettier", -- or "./node_modules/.bin/prettier" if local
+      args = { "--stdin-filepath", "$FILENAME" },
+    },
+  },
+
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
+
+-- Create the :Format command
+vim.api.nvim_create_user_command("Format", function()
+  conform.format()
+end, {})
 
 return options
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,14 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+ map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+
+ -- Ctrl+Backspace to delete the previous word in insert mode;;
+map("i", "<C-H>", "<C-w>")
+
+-- Ctrl+x to delete the entire line in insert mode
+map("i", "<C-x>", "<Esc>ddi")
+
+-- Ctrl+Backspace to delete the previous word in normal mode
+map("n", "<C-BS>", "db")
